f0692c03b9162cbf861946ca310b73798ee44484
fraan05 <alumno.647829@ies-azarquiel.es>
2026-01-04 20:36:22 +0100

    tercer commit

--- main.py
+++ main.py
@@ -18,3 +18,29 @@ class CocheCreate(CocheBase):
 
 class Coche(CocheBase):
     id: int
+
+coches_db: List[Coche] = []
+contador_id = 1
+
+# Crear coches ---------------------
+@app.post("/coches", response_model=Coche, status_code=201)
+def crear_coche(coche: CocheCreate):
+    global contador_id
+
+    # Evitar coches duplicados
+    for c in coches_db:
+        if c.marca.lower() == coche.marca.lower() and c.modelo.lower() == coche.modelo.lower():
+            raise HTTPException(status_code=400, detail="El coche ya existe")
+
+    nuevo_coche = Coche(
+        id=contador_id,
+        **coche.model_dump()
+    )
+    contador_id += 1
+    coches_db.append(nuevo_coche)
+    return nuevo_coche
+
+@app.get("/coches", response_model=List[Coche])
+def obtener_coches():
+    return coches_db
+
